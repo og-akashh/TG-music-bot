c5424e76d6104c0d2ee28c7d8dec0c4e2556e90f
Akash Patel <006akashpatel@gmail.com>
2026-04-17 22:34:17 +0530

    Initialize services in init.py

---
+++ bot/services/init.py
@@ -0,0 +1,13 @@
+from .youtube import YouTubeService
+from .spotify import SpotifyService
+from .lyrics import LyricsService
+from .thumbnails import ThumbnailGenerator
+from .suggestions import SuggestionService
+
+__all__ = [
+    "YouTubeService",
+    "SpotifyService", 
+    "LyricsService",
+    "ThumbnailGenerator",
+    "SuggestionService"
+]
